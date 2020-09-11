3901d23440ad626729fd9257c88d435770bb0e69
Finlay MacGregor <finlay.n.macgregor@gmail.com>
2020-09-11 17:05:20 +0200

    Functional tableView with 3 cells, prints to output when tapped

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -40,6 +40,8 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
+        cell.textLabel?.text = "Hello World"
+        
         return cell
     }
     
