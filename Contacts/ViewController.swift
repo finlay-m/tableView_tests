b1aa7567dfad8fc120d3516fef2e0c9eb636e7f8
Finlay MacGregor <finlay.n.macgregor@gmail.com>
2020-09-11 19:07:49 +0200

    tableView extension functions (numberOfRowsInSection and cellForRowAt) moved inside of the ViewController class. Also changed array to myData.

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -8,18 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// moved delegate and viewdata into this class instead of as separate extension with function
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     // connects tableView object to the code
     @IBOutlet var tableView: UITableView!
     
-    // let declares a constant immutable value
-    let names = [
-        "John Smith",
-        "Dan Smith",
-        "Jason Smith",
-        "Mary Smith"
-    ]
+    let myData = ["first", "second", "third", "fourth", "fifth"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,42 +24,22 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         
     }
-
-
-}
-
-extension ViewController: UITableViewDelegate {
     
-    // prints to output when you tap a button
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("you tapped me!")
-    }
+    // TableView Funcs
     
-}
-
-extension ViewController: UITableViewDataSource {
-    
-    // by default there is only 1 section in a table (think column)
-    // so this func is unnecessary since it's set to return 1
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    // this function defines the number of rows in the section
+    // rows will be equal to the length of the myData array
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // number of rows depends on length of names array
-        return names.count
+        return myData.count
     }
     
-    // this function goes through all the cells, telling the program what to fill them with
+    // function goes through each cell and fills it
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // important that this is "cell" which is the name defined by Table View Cell- identifier
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        // part of the text that specifies that names array fills the cells
-        cell.textLabel?.text = names[indexPath.row]
-        
+        // Fills cells with values from myData array
+        cell.textLabel?.text = myData[indexPath.row]
         return cell
     }
-    
+
+
 }
+
